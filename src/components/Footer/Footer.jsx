b4c9797301e30ed80f862de8bf90212732e983cb
KKD98 <KajolKona98@gmail.com>
2023-06-07 16:56:28 +0600

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import logo from '../../assets/logo.png'
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is Footer</h1>
+        <div className='mt-4'>
+            <footer className="footer p-10 bg-black text-white">
+                <div className='flex flex-col items-center'>
+                    <img className='w-[30%] h-[60%]' src={logo} alt="" />
+                    <p className='text-rose-800 text-2xl font-semibold'>Music Mentor</p>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Legal Notice</a>
+                </div>
+                <div>
+                    <span className="footer-title">Support</span>
+                    <a className="link link-hover">Help Center</a>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                </div>
+                <div>
+                    <span className="footer-title">Social</span>
+                    <div className='flex gap-4 text-4xl'>
+                        <FaFacebook className='bg-blue-700 text-white rounded-full border-none'></FaFacebook>
+                        <FaInstagram className='bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 rounded-lg'></FaInstagram>
+                        <FaTwitter className='bg-white text-sky-500 p-1 rounded-lg'></FaTwitter>
+                    </div>
+                </div>
+            </footer>
+            <footer className="footer footer-center p-4 bg-rose-700 text-white">
+                <div>
+                    <p>Copyright © 2023 - All right reserved by Music Mentor</p>
+                </div>
+            </footer>
         </div>
     );
 };
