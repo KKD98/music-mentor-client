b058199303881c960babb66ff9af3d065e8d2781
KKD98 <KajolKona98@gmail.com>
2023-06-08 17:04:18 +0600

    popular classes section design done

--- src/components/pages/Home/PopularClasses/PopularClasses.jsx
+++ src/components/pages/Home/PopularClasses/PopularClasses.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SingleClass from '../SingleClass/SingleClass';
 
 const PopularClasses = () => {
+    const [classes , setClasses] = useState([]);
+    useEffect(() => {
+        fetch('class.json')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setClasses(data)
+        })
+    },[])
     return (
         <div>
-            <p>Popular classes</p>
+            <h1 className='text-center text-4xl font-semibold mt-12 mb-4'>---Popular Classes of <span className='text-rose-700'>MUSIC MENTOR</span>
+            ---</h1>
+        
+           <div className='grid grid-cols-1 lg:grid-cols-3 gap-3'>
+           {
+                classes.map(classItem => <SingleClass key={classItem._id} classItem={classItem}></SingleClass>)
+            }
+           </div>
         </div>
     );
 };
---
+++ src/components/pages/Home/SingleClass/SingleClass.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SingleClass = ({ classItem }) => {
+    const { className, classImage, amount,  enroll_total_student, instructorName, totalStudent } = classItem;
+    return (
+        <div className="card w-full bg-black text-white shadow-xl">
+            <figure className="px-0 pt-0">
+                <img  src={classImage} />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{className}</h2>
+                <div className='flex gap-3 justify-between w-full my-2'>
+                <p>Amount: ${amount}</p>
+                <p>Total Student: {totalStudent}</p>
+                </div>
+                <div className="card-actions">
+                   <Link to="/classes"> <button className="btn bg-rose-700 border-none text-white hover:bg-rose-900">See All Classes</button></Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SingleClass;
