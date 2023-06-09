62279c22389b2302087fcd1ef8639ee7b98dffdd
KKD98 <KajolKona98@gmail.com>
2023-06-09 21:46:58 +0600

    instructors ui done

--- src/components/pages/Instructors/Instructors.jsx
+++ src/components/pages/Instructors/Instructors.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 
 const Instructors = () => {
+    const [instructors, setInstructors] = useState([]);
+
+    // TODO
+    useEffect(() => {
+        fetch('/instructor.json')
+            .then(res => res.json())
+            .then(data => {
+                setInstructors(data);
+            })
+            .catch(error => {
+                console.log('Error fetching data:', error);
+            });
+    }, []);
+
+    console.log(instructors);
     return (
-        <div>
-        <Helmet>
-            <title>Instructors | MusicMentor</title>
-        </Helmet>
-            <p>Instructors page</p>
+        <div className='w-full pt-16'>
+            <Helmet>
+                <title>MyClasses | MusicMentor</title>
+            </Helmet>
+            <h1 className='text-center text-4xl text-rose-700 font-semibold my-4'>My All Classes</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-4 gap-3'>
+            {
+                instructors.map(instructor =>
+                    <div key={instructor._id} className="w-full h-full relative bg-black text-white shadow-xl">
+                        <figure><img className='p-16 mb-6 rounded-full' src={instructor.instructorImage} alt="Shoes" /></figure>
+                        <div className="bg-rose-700 p-3 absolute inset-x-0 bottom-0">
+                            <h2 className="text-2xl font-semibold">{instructor.name}</h2>
+                            <h2 className="text-xl font-semibold">{instructor.email}</h2>
+                        </div>
+                    </div>
+                )
+            }
+            </div>
         </div>
     );
 };
