721f1c120d83f049eca52e35f2ef7a0ebf120636
KKD98 <KajolKona98@gmail.com>
2023-06-09 13:12:19 +0600

    Manage classes ui done

--- src/components/pages/DashBoard/AdminDashBoard/ManageAllClasses/ManageAllClasses.jsx
+++ src/components/pages/DashBoard/AdminDashBoard/ManageAllClasses/ManageAllClasses.jsx
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ManageAllClasses = () => {
+    const [classes, setClasses] = useState([]);
+
+    // TODO
+    useEffect(() => {
+        fetch('/allClasses.json')
+            .then(res => res.json())
+            .then(data => {
+                setClasses(data);
+            })
+            .catch(error => {
+                console.log('Error fetching data:', error);
+            });
+    }, []);
+
+    console.log(classes);
+
     return (
         <div>
-            <p>Manage all classes</p>
+            <p className='text-4xl text-center font-semibold text-rose-700'>Total Classes: {classes.length}</p>
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>ClassImage</th>
+                            <th>ClassName</th>
+                            <th>InstructorName</th>
+                            <th>InstructorEmail</th>
+                            <th>Available Seats</th>
+                            <th>Price</th>
+                            <th>Status</th>
+                            <th className='text-center'>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {classes.map(singleClass =>
+                            <tr key={singleClass._id}>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={singleClass.classImage} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>{singleClass.className}</td>
+                                <td>{singleClass.instructorName}</td>
+                                <td>{singleClass.instructorEmail}</td>
+                                <td>{singleClass.available_seats}</td>
+                                <td>{singleClass.price}</td>
+                                <td>{singleClass.status}</td>
+                                <th>
+                                    <div className='flex gap-2'>
+                                        <button className="btn bg-rose-700 text-white btn-xs">Approved</button>
+                                        <button className="btn bg-rose-700 text-white btn-xs">Deny</button>
+                                        <button className="btn bg-rose-700 text-white btn-xs">Feedback</button>
+                                    </div>
+                                </th>
+                            </tr>
+                        )}
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from '../firebase/firebase.config';
+import axios from 'axios';
 
 
 export const AuthContext = createContext(null);
@@ -40,7 +41,21 @@ const AuthProvider = ({children}) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth , currentUser => {
             setUser(currentUser);
-            console.log('Current-User:' , currentUser)
+            console.log('Current-User:' , currentUser);
+
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt' , {email: currentUser.email})
+                .then(data => {
+                    // console.log(data.data.token)
+                    // TODO
+                    localStorage.setItem('access-token', data.data.token);
+                })
+            }
+
+            else{
+                localStorage.removeItem('access-token');
+            }
+
             setLoading(false);
         });
         return () => {
