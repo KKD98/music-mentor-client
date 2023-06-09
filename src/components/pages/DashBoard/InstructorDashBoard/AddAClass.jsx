1b73ee2202b3a41816b62c23046661be9e5922e1
KKD98 <KajolKona98@gmail.com>
2023-06-09 21:08:43 +0600

    My Classes ui done

--- src/components/pages/DashBoard/InstructorDashBoard/AddAClass.jsx
+++ src/components/pages/DashBoard/InstructorDashBoard/AddAClass.jsx
@@ -1,47 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useForm } from 'react-hook-form';
+import { AuthContext } from '../../../../providers/AuthProvider';
 
-const image_hosting_token=import.meta.env.VITE_IMAGE_UPLOAD_TOKEN;
+const image_hosting_token = import.meta.env.VITE_IMAGE_UPLOAD_TOKEN;
+
 
 const AddAClass = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const image_hosting_url = `https://api.imgbb.com/1/upload?key=${image_hosting_token}`;
+    const { user } = useContext(AuthContext);
+    const {displayName, email, photoURL} = user;
+    console.log(user)
 
-  const onSubmit = (data) => {
-    console.log(data);
-    const formData = new FormData();
-    formData.append('image', data.class_image[0])
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const image_hosting_url = `https://api.imgbb.com/1/upload?key=${image_hosting_token}`;
 
-    fetch(image_hosting_url, {
-      method: 'POST',
-      body: formData
-    })
-      .then(res => res.json())
-      .then(imgRes => {
-        if(imgRes.success){
-            const imgURL = imgRes.data.display_url;
-            const classItem = data;
-            classItem.class_image = imgURL;
-            console.log(classItem)
-        }
-      });
-  };
+    const onSubmit = (data) => {
+        console.log(data);
+        const formData = new FormData();
+        formData.append('image', data.class_image[0])
 
-  return (
-            <div className='w-full'>
-                <Helmet>
-                    <title>AddAClass | MusicMentor</title>
-                </Helmet>
-                <p className='text-rose-700 text-4xl text-center font-semibold my-4'>Add a class</p>
-                
-                <div className='flex justify-center'>
+        fetch(image_hosting_url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgRes => {
+                if (imgRes.success) {
+                    const imgURL = imgRes.data.display_url;
+                    const { class_name, available_seats, price } = data;
+                    const newClass = { instructor_name:displayName, instructor_email: email, instructor_image: photoURL, class_name, class_image: imgURL, available_seats: parseInt(available_seats), price: parseFloat(price), status: "pending" };
+                    console.log(newClass)
+                }
+            });
+    };
+
+    return (
+        <div className='w-full'>
+            <Helmet>
+                <title>AddAClass | MusicMentor</title>
+            </Helmet>
+            <p className='text-rose-700 text-4xl text-center font-semibold my-4'>Add a class</p>
+
+            <div className='flex justify-center'>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
                             <span className="label-text">Select Class Name*</span>
                         </label>
-                        <select defaultValue="Pick one" className="select select-bordered"  {...register("class_name", {required: true})}>
+                        <select defaultValue="Pick one" className="select select-bordered"  {...register("class_name", { required: true })}>
                             <option disabled>Pick one</option>
                             <option>Guitar</option>
                             <option>Piano</option>
@@ -58,35 +64,35 @@ const AddAClass = () => {
                             <span className="label-text">Class Image*</span>
                         </label>
                         <input type="file" className="file-input file-input-bordered w-full max-w-xs"
-                             {...register("class_image", {required: true})}
+                            {...register("class_image", { required: true })}
                         />
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
                             <span className="label-text">Available Seats*</span>
                         </label>
-                        <input type="text" placeholder="Available Seats" className="input input-bordered w-full max-w-xs" 
-                            {...register("available_seats", {required: true})}
+                        <input type="text" placeholder="Available Seats" className="input input-bordered w-full max-w-xs"
+                            {...register("available_seats", { required: true })}
                         />
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
                             <span className="label-text">Price*</span>
                         </label>
-                        <input type="text" placeholder="Price" className="input input-bordered w-full max-w-xs" 
-                             {...register("price", {required: true})}
+                        <input type="text" placeholder="Price" className="input input-bordered w-full max-w-xs"
+                            {...register("price", { required: true })}
                         />
                     </div>
-                   <div className='my-4 flex justify-center'>
-                   <input className='btn btn-sm bg-rose-700 hover:bg-rose-900 text-white ' type="submit" value="Add Class" />
-                   </div>
+                    <div className='my-4 flex justify-center'>
+                        <input className='btn btn-sm bg-rose-700 hover:bg-rose-900 text-white ' type="submit" value="Add Class" />
+                    </div>
                 </form>
-                </div>
             </div>
-        );
-    };
-    
-    export default AddAClass;
+        </div>
+    );
+};
+
+export default AddAClass;
 
 
 
--- src/components/pages/DashBoard/InstructorDashBoard/MyClasses.jsx
+++ src/components/pages/DashBoard/InstructorDashBoard/MyClasses.jsx
@@ -1,9 +1,66 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const MyClasses = () => {
+    const [classes, setClasses] = useState([]);
+
+    // TODO
+    useEffect(() => {
+        fetch('/allClasses.json')
+            .then(res => res.json())
+            .then(data => {
+                setClasses(data);
+            })
+            .catch(error => {
+                console.log('Error fetching data:', error);
+            });
+    }, []);
+
+    console.log(classes);
     return (
-        <div>
-            <p className='text-2xl'>My all classes</p>
+        <div className='w-full'>
+            <Helmet>
+                <title>MyClasses | MusicMentor</title>
+            </Helmet>
+            <h1 className='text-center text-4xl text-rose-700 font-semibold my-4'>My All Classes</h1>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>ClassImage</th>
+                            <th>ClassName</th>
+                            <th>Price</th>
+                            <th>Status</th>
+                            <th>Feedback</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {classes.map(myClass => 
+                            <tr key={myClass._id}>
+                            <td>
+                                <div className="flex items-center space-x-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={myClass.classImage} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                               {myClass.className}
+                            </td>
+                            <td>{myClass.price}</td>
+                            <td>{myClass.status}</td>
+                            <td>{myClass.feedback? myClass.feedback : "No feedback"}</td>
+                        </tr>
+                        )}
+                       
+                       </tbody>
+                </table>
+            </div>
         </div>
     );
 };
