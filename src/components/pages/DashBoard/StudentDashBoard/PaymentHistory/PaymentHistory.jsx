c2ca9283f4016a44bc786176953edf96d26e36b5
KKD98 <KajolKona98@gmail.com>
2023-06-11 13:08:44 +0600

    payment history done

--- src/components/pages/DashBoard/StudentDashBoard/PaymentHistory/PaymentHistory.jsx
+++ src/components/pages/DashBoard/StudentDashBoard/PaymentHistory/PaymentHistory.jsx
@@ -21,9 +21,47 @@ const PaymentHistory = () => {
             });
     }, []);
     return (
-        <div>
-            <p>payment history: {paymentedClasses.length}</p>
+        <div className='w-full'>
+        <p className='text-center text-4xl text-rose-700 font-semibold my-4'>My Payments</p>
+        <div className="overflow-x-auto">
+            <table className="table">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th>Class Image</th>
+                        <th>Class Name</th>
+                        <th>Instructor Name</th>
+                        <th>Price</th>
+                        <th>Transaction Id</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {paymentedClasses.map(classItem =>
+                        <tr key={classItem._id}>
+                            <td>
+                                <div className="flex items-center space-x-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={classItem.class_image} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                {classItem.class_name}
+                            </td>
+                            <td>{classItem.instructor_name}</td>
+                            <td>${classItem.price}</td>
+                            <td>{classItem.transactionId}</td>
+                        </tr>
+                    )}
+
+
+                </tbody>
+
+            </table>
         </div>
+    </div>
     );
 };
 
--- src/components/pages/SingleClass/SingleClass.jsx
+++ src/components/pages/SingleClass/SingleClass.jsx
@@ -55,11 +55,11 @@ const SingleClass = ({classItem}) => {
     }
 
     const cardStyle = {
-        backgroundColor: available_seats === 0 ? 'red-700' : 'black'
+        backgroundColor: available_seats === 0 ? '#FF0000' : '#000000'
       };
     
     return (
-        <div key={_id} className={`card w-full bg-${cardStyle.backgroundColor} text-white shadow-xl`}>
+        <div key={_id} className="card w-full text-white shadow-xl" style={{ backgroundColor: cardStyle.backgroundColor }}>
             <figure><img className='p-5 w-full h-64' src={class_image} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{class_name} Class</h2>
